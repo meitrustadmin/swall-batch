784ec15bdc6c4336cf36e135756c2dc141e6440e
LEI WANG <william.wang@meitrust.com>
2024-11-22 09:41:32 +0800

    solve build error

--- pages/api/hello/route.ts
+++ pages/api/hello/route.ts
@@ -2,5 +2,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
+    console.log(request.headers);
   return new Response(`Hello from batch job test`);
 }
